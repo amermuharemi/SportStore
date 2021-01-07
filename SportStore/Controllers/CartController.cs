6db36e41117ae438f8600ef3de3f3f8f6f09fe58
Amer Muharemi <amer.muharemi@riinvest.net>
2021-01-07 21:25:14 +0100

    Added filtering to the dashboard

--- SportStore/Controllers/CartController.cs
+++ SportStore/Controllers/CartController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SportStore.Infrastructure;
 using SportStore.Interfaces;
 using SportStore.Models;
+using SportStore.Models.ViewModels;
 
 namespace SportStore.Controllers
 {
@@ -14,7 +17,16 @@ namespace SportStore.Controllers
         {
             repo = _repo;
         }
-        
+
+        public ViewResult Index(string returnURl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = new Cart(),
+                ReturnUrl = returnURl
+            });
+        } 
+
         public RedirectToActionResult AddToCart(int productId,string returnUrl)
         {
             Product product = repo.Products
@@ -23,13 +35,37 @@ namespace SportStore.Controllers
             if (product == null)
             {
                 Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
             }
+
+            return RedirectToAction("Index", new { returnUrl });
         }
+
+        public RedirectToActionResult RemoveFromCart(int productID, string returnUrl)
+        {
+            Product product = repo.Products
+                .FirstOrDefault(x => x.ProductID == productID);
+
+            if (product == null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
 
         private Cart GetCart()
         {
             Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
             return cart;
+        }
+
+        public void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
