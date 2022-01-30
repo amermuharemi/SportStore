2d842600f2b608f201f3ff61378eec3501fe0ff4
Amer Muharemi <amuharemi@i-aps.com>
2022-01-30 22:03:16 +0100

    Test commit

--- SportStore/Data/AppDbContext.cs
+++ SportStore/Data/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace SportStore.Data
 {
     public class AppDbContext : DbContext
     {
+        //comment to test  for git
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
         {
 
